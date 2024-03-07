68d88c98ceb4558e58658497f13363e1dcb9241b
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-03-07 07:46:54 +0000

    feat: :tada: Init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TallySurvey from "./TallySurvey";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <TallySurvey />
+    </div>
+  );
+}
+
+export default App;
